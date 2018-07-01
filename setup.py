9bf278d25536dece691b722fb9f582a365dc13a6
guyskk <guyskk@qq.com>
2018-07-01 13:05:02 +0800

    release 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='weirb',
-    version='0.2.0',
+    version='0.1.0',
     description='Weird Web Framework',
     url='https://github.com/guyskk/weirb',
     author='guyskk',
--- tasks.py
+++ tasks.py
@@ -1,6 +1,15 @@
+from pathlib import Path
 from invoke import task
 
 
 @task
 def build(ctx):
+    for f in Path('dist').glob('*'):
+        f.unlink()
     ctx.run('python setup.py sdist')
+
+
+@task
+def publish(ctx):
+    build(ctx)
+    ctx.run('twine upload dist/*')
