dc91b4d92ca5fad36ddc86b03db2da32f8915a4d
guyskk <guyskk@qq.com>
2018-07-02 22:30:16 +0800

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='weirb',
-    version='0.2.1',
+    version='0.2.2',
     description='Weird Web Framework',
     url='https://github.com/guyskk/weirb',
     author='guyskk',
